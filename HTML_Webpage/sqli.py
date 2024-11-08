585f48ea786b3f3157c012935c7c224ca17fad4f
WhisperingDROID <cadekelley99@gmail.com>
2024-11-08 14:11:28 -0700

    Updated project CRUD for Vehicle Table

--- HTML_Webpage/sqli.py
+++ HTML_Webpage/sqli.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-# Credentials are sepererated using a .env file.
-
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector, os
@@ -10,7 +8,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Passwords are not hard coded
 def get_db_connection():
     connection = mysql.connector.connect(
         host=os.environ.get('SQL_HOST'),
@@ -21,22 +18,60 @@ def get_db_connection():
     )
     return connection
 
-# Vehicle Table
 @app.route('/')
 def showTable():
     connection = get_db_connection()
-    cursor = connection.cursor()
-
+    cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM Vehicle")
     vehicles = cursor.fetchall()
     cursor.close()
     connection.close()
-
     return render_template('index.html', vehicles=vehicles)
 
-# Connection to DB and Form Submission
-@app.route('/add_vehicle', methods=['POST'])
-def add_vehicle():
+@app.route('/submit_add_vehicle', methods=['GET', 'POST'])
+def submit_add_vehicle():
+    if request.method == 'POST':
+        make = request.form['make']
+        model = request.form['model']
+        year = request.form['year']
+        vin_number = request.form['vin_number']
+        license_plate = request.form['license_plate']
+        person_id = request.form['person_id']
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO Vehicle (Make, Model, Year, VIN_Number, LicensePlate, PersonID) VALUES (%s, %s, %s, %s, %s, %s)", 
+                       (make, model, year, vin_number, license_plate, person_id))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        
+        return redirect(url_for('showTable'))
+    
+    return render_template('add_vehicle.html')
+
+@app.route('/delete_vehicle/<int:vehicle_id>', methods=['POST'])
+def delete_vehicle(vehicle_id):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Vehicle WHERE VehicleID = %s", (vehicle_id,))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return redirect(url_for('showTable'))
+
+@app.route('/update_vehicle/<int:vehicle_id>', methods=['GET'])
+def update_vehicle(vehicle_id):
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Vehicle WHERE VehicleID = %s", (vehicle_id,))
+    vehicle = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return render_template('update_vehicle.html', vehicle=vehicle)
+
+@app.route('/submit_update/<int:vehicle_id>', methods=['POST'])
+def submit_update(vehicle_id):
     make = request.form['make']
     model = request.form['model']
     year = request.form['year']
@@ -46,62 +81,14 @@ def add_vehicle():
 
     connection = get_db_connection()
     cursor = connection.cursor()
-
-    # Check to see if PersonID is valid from the Person Table
-    cursor.execute("SELECT * FROM person WHERE PersonID = %s", (person_id,))
-    person = cursor.fetchone()
-
-    # Error if PersonID is not found
-    if not person:
-        cursor.close()
-        connection.close()
-        return "Error: Please provide a valid PersonID. (1 or 2)", 400
-
-    # Insert the new vehicle using the provided data (only if the PersonID exists)
-    query = """
-    INSERT INTO Vehicle (Make, Model, Year, VIN_Number, LicensePlate, PersonID)
-    VALUES (%s, %s, %s, %s, %s, %s)
-    """
-    cursor.execute(query, (make, model, year, vin_number, license_plate, person_id))
+    cursor.execute("""
+        UPDATE Vehicle 
+        SET Make = %s, Model = %s, Year = %s, VIN_Number = %s, LicensePlate = %s, PersonID = %s
+        WHERE VehicleID = %s
+    """, (make, model, year, vin_number, license_plate, person_id, vehicle_id))
     connection.commit()
-
     cursor.close()
     connection.close()
-
-    # Redirection back to home page
-    return redirect(url_for('showTable'))
-    
-
-# I added a delete function in case the user makes a mistake while entering the information. 
-# It is also useful for updating the database later on if needed.
-
-@app.route('/delete_vehicle/<int:vehicle_id>', methods=['POST'])
-def delete_vehicle(vehicle_id):
-    connection = get_db_connection()
-    cursor = connection.cursor()
-
-    # Delete the vehicle with the given VehicleID
-    cursor.execute("DELETE FROM Vehicle WHERE VehicleID = %s", (vehicle_id,))
-    connection.commit()
-
-    cursor.close()
-    connection.close()
-
-    # Redirect back to the home page after deletion
-    return redirect(url_for('showTable'))
-
-
-    # SQL Injection Fix
-    query = """
-    INSERT INTO Vehicle (Make, Model, Year, VIN_Number, LicensePlate, PersonID)
-    VALUES (%s, %s, %s, %s, %s, %s)
-    """
-    cursor.execute(query, (make, model, year, vin_number, license_plate, person_id))
-    connection.commit()
-
-    cursor.close()
-    connection.close()
-
     return redirect(url_for('showTable'))
 
 if __name__ == '__main__':
